468da49a2b273649351436312eebf236c6546404
s3979194 <s3979194@rmit.edu.vn>
2024-01-12 20:45:32 +0700

    commit changes

--- include/User.h
+++ include/User.h
@@ -2,7 +2,8 @@
 #define USER_H
 
 #include <string>
-
+#include <vector>
+using namespace std;
 class User {
 public:
     User(const std::string& username, const std::string& password, const std::string& fullName, const std::string& phoneNumber, const std::string& email, const std::string& address, int creditPoints);
@@ -14,7 +15,13 @@ public:
     const std::string& getAddress() const;
     int getCreditPoints() const;
 
-    bool login(const std::string& enteredPassword) const;
+    const std::string& setUsername() const;
+    const std::string& setFullName() const;
+    const std::string& setPhoneNumber() const;
+    const std::string& setEmail() const;
+    int setCreditPoints() const;
+
+    bool login(const std::string& enteredPassword) const ;
     void viewInformation() const;
     void registerAccount(const std::string& newPassword, const std::string& newFullName, const std::string& newPhoneNumber, const std::string& newEmail, const std::string& newAddress);
     void resetPassword(const std::string& newPassword);
